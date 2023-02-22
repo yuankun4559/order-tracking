62dd6c0de727dfa4723e71b96b065057597e2cd6
yuankun <yuankun0310@126.com>
2023-02-22 17:33:26 +0800

    fix: 修复本地模拟接口数据

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,14 @@
 // 运行时配置
 import type { RequestConfig, RuntimeConfig } from '@umijs/max';
 import { message } from 'antd';
-import authorityService from '@/services/authority';
-const { fetchUserRole } = authorityService.AuthorityController;
+// import authorityService from '@/services/authority';
+// const { fetchUserRole } = authorityService.AuthorityController;
 
 // 全局初始化数据配置，用于 Layout 用户信息和权限初始化
 // 更多信息见文档：https://next.umijs.org/docs/api/runtime-config#getinitialstate
 export async function getInitialState() {
-  let data = [];
-  try {
-    // 查询用户权限
-    data = await fetchUserRole();
-  } catch (error) {
-    console.log('error', error);
-  }
+  // const data = await fetchUserRole()
+  let data = ['auth:read', 'auth:download'];
   return {
     authoriy: data,
     count: 10,
